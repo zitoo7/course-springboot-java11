856d534ea64d33d0bdfb67daa048da0db5149fcb
João Soares <zitto_7@hotmail.com>
2021-12-12 18:23:14 +0000

    Many-to-many association with JoinTable (Product and Category)

--- src/main/java/com/courseJava/course/entities/Category.java
+++ src/main/java/com/courseJava/course/entities/Category.java
@@ -1,5 +1,7 @@
 package com.courseJava.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.*;
@@ -13,7 +15,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    @Transient
+    @ManyToMany(mappedBy = "categories")
+    @JsonIgnore
     private Set<Product> products = new HashSet<>();
 
     public Category(){}
--- src/main/java/com/courseJava/course/entities/Product.java
+++ src/main/java/com/courseJava/course/entities/Product.java
@@ -1,5 +1,7 @@
 package com.courseJava.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
@@ -18,7 +20,10 @@ public class Product implements Serializable {
     private Double price;
     private String imgUrl;
 
-    @Transient
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
     public Product(){}
@@ -71,6 +76,10 @@ public class Product implements Serializable {
         this.imgUrl = imgUrl;
     }
 
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -82,9 +91,5 @@ public class Product implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hash(id);
-    }
-
-    public Set<Category> getCategories() {
-        return categories;
     }
 }
