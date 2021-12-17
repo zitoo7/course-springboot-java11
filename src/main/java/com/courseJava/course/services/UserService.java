74ecffd9eae3f601eaedafa70796d63506c5ae7f
João Soares <zitto_7@hotmail.com>
2021-12-17 20:13:12 +0000

    Exception handling - findById Exception handling - delete Exception handling - update

--- src/main/java/com/courseJava/course/services/UserService.java
+++ src/main/java/com/courseJava/course/services/UserService.java
@@ -2,9 +2,14 @@ package com.courseJava.course.services;
 
 import com.courseJava.course.entities.User;
 import com.courseJava.course.repositories.UserRepository;
+import com.courseJava.course.services.exceptions.DatabaseException;
+import com.courseJava.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,21 +26,32 @@ public class UserService {
 
     public User findById(Long id){
        Optional<User> obj = repository.findById(id);
-       return obj.get();
+       return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
         return repository.save(obj);
     }
 
-    public void delete(Long id){
-        repository.deleteById(id);
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User obj) {
---
+++ src/main/java/com/courseJava/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.courseJava.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+    public DatabaseException(String msg){
+        super(msg);
+    }
+}
